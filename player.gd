e1d3f1dcde8145d12fd22f9525e42e22e82d2d93
emredurak01 <emredurak01@gmail.com>
2024-03-08 14:03:24 +0300

    Movement

---
+++ player.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+
+const SPEED = 100.0
+const ACCELERATION =  800.0
+const FRICTION = 1000.0
+const JUMP_VELOCITY = -300.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var animated_sprite_2d = $CollisionShape2D
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = move_toward(velocity.x, SPEED*direction, ACCELERATION*delta)
+	else:
+		velocity.x = move_toward(velocity.x, 0, FRICTION*delta)
+
+	move_and_slide()
