0cc62643d1cc1688c7e1fa8d0d4363e34470f802
solaire <timur.ozkul.ac@gmail.com>
2024-03-08 20:13:04 +0000

    grab and throw fix

--- Scripts/obstacle_past.gd
+++ Scripts/obstacle_past.gd
@@ -9,6 +9,7 @@ func _physics_process(delta):
 
 func _input(event):
 	if Input.is_action_just_pressed("ui_pick") and picked == false:
+		self.angular_velocity = 0
 		var bodies = $Area2D.get_overlapping_bodies()
 		for body in bodies:
 			if body.name == "Player" and get_node("../Player").can_pick == true:
@@ -16,18 +17,28 @@ func _input(event):
 				get_node("../Player").can_pick = false
 
 	elif Input.is_action_just_pressed("ui_drop") and picked == true:
-		self.set_physics_process(true)
+		self.freeze = true
+		await get_tree().create_timer(0.1).timeout 
+		self.freeze = false
+		self.linear_velocity.y = 0
+		self.linear_velocity.x = get_node("../Player").velocity.x * 1.5
 		picked = false
 		get_node("../Player").can_pick = true
-		if get_node("../Player").get_node("../Player/AnimatedSprite2D").flip_h == false:
-			apply_impulse(Vector2(), Vector2(20, -5))
-		else:
-			apply_impulse(Vector2(), Vector2(-20, -5))
+		print("Dropping - Position:", self.position, " Velocity:", self.linear_velocity)
 			
 	elif Input.is_action_just_pressed("ui_throw") and picked == true:
 		picked = false
 		get_node("../Player").can_pick = true
 		if get_node("../Player").get_node("../Player/AnimatedSprite2D").flip_h == false:
+			self.linear_velocity.x = 250
+			self.linear_velocity.y = -250
+			self.angular_velocity = 10
+			self.inertia = 100
 			apply_impulse(Vector2(), Vector2(150, -200))
 		else:
+			self.linear_velocity.x = -250
+			self.linear_velocity.y = -250
+			self.angular_velocity = 10
+			self.inertia = 100
 			apply_impulse(Vector2(), Vector2(-150, -200))
+		print("Throwing - Position:", self.position, " Velocity:", self.linear_velocity)
