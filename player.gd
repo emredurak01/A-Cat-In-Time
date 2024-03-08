db0b6499c02aaceb5ae2b2960feac418f8bada30
Timur <timur@Shalmon.local>
2024-03-08 12:46:47 +0000

    Anim basics

--- player.gd
+++ player.gd
@@ -5,27 +5,59 @@ const SPEED = 100.0
 const ACCELERATION =  800.0
 const FRICTION = 1000.0
 const JUMP_VELOCITY = -300.0
+const CROUCH_SPEED = 50.0
+const CROUCH_ACCELERATION = 400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite_2d = $CollisionShape2D
+@onready var sprite_animate = $AnimatedSprite2D #for animation
+
+var allow_jump = true
+var crouch_state = true
+
+func _ready():
+	sprite_animate.play("idle") #idle animation on boot
 
 func _physics_process(delta):
 	# Add the gravity.
+	#Also modifies so the cat doesnt jump infinitely (singular)
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+		allow_jump = false
+	else:
+		allow_jump = true
+		
+	#crouch handling
+	if Input.is_action_pressed("ui_down"):
+		crouch_state = true
+		velocity.x = move_toward(velocity.x, 0, CROUCH_ACCELERATION * delta)
+	else:
+		crouch_state = false
+		
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = move_toward(velocity.x, SPEED*direction, ACCELERATION*delta)
+		if crouch_state:
+			velocity.x = move_toward(velocity.x, CROUCH_SPEED*direction, CROUCH_ACCELERATION*delta)
+			sprite_animate.play("crouch")
+			sprite_animate.flip_h = direction < 0
+		else:
+			velocity.x = move_toward(velocity.x, SPEED*direction, ACCELERATION*delta)
+			sprite_animate.play("run")
+			sprite_animate.flip_h = direction < 0 #direction turn when moving in opposed direction
 	else:
-		velocity.x = move_toward(velocity.x, 0, FRICTION*delta)
+		if crouch_state:
+			velocity.x = move_toward(velocity.x, 0, CROUCH_ACCELERATION*delta)
+			sprite_animate.play("crouch")
+		else:
+			velocity.x = move_toward(velocity.x, 0, FRICTION*delta)
+			sprite_animate.play("idle")
 
 	move_and_slide()
