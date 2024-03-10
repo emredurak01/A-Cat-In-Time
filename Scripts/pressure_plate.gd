6448597308cd767c3cfc6eac2e2c048000eabf71
Emre Durak <emredurak01@gmail.com>
2024-03-11 02:01:56 +0300

    pressure plate

--- Scripts/pressure_plate.gd
+++ Scripts/pressure_plate.gd
@@ -2,15 +2,27 @@ extends StaticBody2D
 
 var pressed = false
 
+func _ready():
+	$green.visible = false
+	$red.visible = true
+
 func _on_area_2d_area_entered(area):
 	if($Area2D.get_overlapping_areas() != null):
 		pressed = true
+		$green.visible = true
+		$red.visible = false
 	if($Area2D.get_overlapping_areas() == null):
 		pressed = false
+		$green.visible = false
+		$red.visible = true
 
 func _on_area_2d_area_exited(area):
+	
 	if($Area2D.get_overlapping_areas() != null):
-		pressed = true
-		
-	if($Area2D.get_overlapping_areas() == null):
 		pressed = false
+		$green.visible = false
+		$red.visible = true
+	if($Area2D.get_overlapping_areas() == null):
+		pressed = true
+		$green.visible = true
+		$red.visible = false
