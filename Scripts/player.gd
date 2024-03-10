76cf5ac5a2f39f9037a4a5c3ac8cf80c99540a20
solaire <timur.ozkul.ac@gmail.com>
2024-03-10 14:25:24 +0000

    time leap anim

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -18,12 +18,13 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var teleporting_with_past_object = false
 var teleporting_with_future_object = false
 var allow_jump = true
-var crouch_state = true
 var jump_state = true
 var future_state = false
 var can_pick = true
 var can_leap: bool = true
 var teleporting = false
+var just_teleported = false
+var allow_teleport = true
 
 var held_obstacle_past : RigidBody2D = null
 var held_obstacle_future : RigidBody2D = null
@@ -35,25 +36,21 @@ func _ready():
 	sprite_animate.play("idle") #idle animation on boot
 
 func _physics_process(delta):
-	
+	if just_teleported: 
+		sprite_animate.play("wake_up")
+		await get_tree().create_timer(1.6).timeout
 	# Add the gravity.
 	#Also modifies so the cat doesnt jump infinitely (singular)
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		allow_jump = false
+		allow_teleport = false
 		if velocity.y < 0: sprite_animate.play("jumping_up")
 		else: sprite_animate.play("falling_down")
 	else:
 		jump_state = false
 		allow_jump = true
-		
-	#crouch handling
-	if Input.is_action_pressed("ui_down"):
-		crouch_state = true
-		velocity.x = move_toward(velocity.x, 0, CROUCH_ACCELERATION * delta)
-		#sprite_animate.play("crouch")
-	else:
-		crouch_state = false
+		allow_teleport = true
 		
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
@@ -61,63 +58,49 @@ func _physics_process(delta):
 			jump_state = true
 			velocity.y = JUMP_VELOCITY
 
-	
 	#Time travel
-	if Input.is_action_just_pressed("ui_accept") and can_leap:
+	if Input.is_action_just_pressed("ui_accept") and can_leap and allow_teleport:
+		sprite_animate.play("sleep")
 		teleporting = true
 		time_shader.material.set_shader_parameter("chaos",32)
-		
-		await get_tree().create_timer(1).timeout
-		
-		can_leap = false
 		$TLCooldown.start()
+		can_leap = false
+		await get_tree().create_timer(1.6).timeout
+		
+		just_teleported = true
 		if future_state == false:
 			position.y += 255
 			camera.limit_top = 255
 			camera.limit_bottom = 255
 			future_state = true
-
 			if(held_obstacle_past != null):
 				teleporting_with_past_object = true
 			if(held_obstacle_future != null):
 				teleporting_with_future_object = true
 			teleporting = false
-			await get_tree().create_timer(1).timeout
+			await get_tree().create_timer(1.6).timeout
 			time_shader.material.set_shader_parameter("chaos",0)
-			
 		else:
 			position.y -= 255
 			camera.limit_top = 0
 			camera.limit_bottom = 0
 			future_state = false
-			time_shader.material.set_shader_parameter("chaos",0)
 			if(held_obstacle_past != null):
 				teleporting_with_past_object = true
 			if(held_obstacle_future != null):
 				teleporting_with_future_object = true
-				
 			teleporting = false
-			await get_tree().create_timer(1).timeout
+			await get_tree().create_timer(1.6).timeout
 			time_shader.material.set_shader_parameter("chaos",0)
+		just_teleported = false
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if (!teleporting):
 		if direction:
-			if crouch_state:
-				velocity.x = move_toward(velocity.x, CROUCH_SPEED*direction, CROUCH_ACCELERATION*delta)
-				if !jump_state: sprite_animate.play("crouch")
-				sprite_animate.flip_h = direction < 0
-			else:
 				velocity.x = move_toward(velocity.x, SPEED*direction, ACCELERATION*delta)
 				if !jump_state: sprite_animate.play("run")
-				sprite_animate.flip_h = direction < 0 #direction turn when moving in opposed direction
+				sprite_animate.flip_h = direction < 0
 		else:
-			if crouch_state:
-				velocity.x = move_toward(velocity.x, 0, CROUCH_ACCELERATION*delta)
-				if !jump_state: sprite_animate.play("crouch")
-			else:
 				velocity.x = move_toward(velocity.x, 0, FRICTION*delta)
 				if !jump_state: sprite_animate.play("idle")
 				
