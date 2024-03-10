6796a9659a60d2fae52438264cd6005068c8ab09
Emre Durak <emredurak01@gmail.com>
2024-03-11 02:57:35 +0300

    add ambience

--- Prefab/door.gd
+++ Prefab/door.gd
@@ -2,9 +2,7 @@ extends StaticBody2D
 
 func _process(delta):
 	if(find_child("pressure_plate").pressed):
-		#print("açık")
 		set_collision_layer_value(2, false)
 	else:
-		#print("kapalı")
 		set_collision_layer_value(2, true)
 	
