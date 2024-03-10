47a7ce1e7db89e216f17cf3fdef0a99c7dd8c5a0
BerkBerberoglu <berkberberoglu3@gmail.com>
2024-03-10 20:17:42 +0300

    Tiles Final

--- Scripts/world.gd
+++ Scripts/world.gd
@@ -4,14 +4,11 @@ extends Node2D
 @onready var player = $Player
 @onready var camera = $Player/Camera2D
 @onready var pause_menu = $Player/Camera2D/PauseMenu
-@onready var collision_polygon_2d = $Past/CollisionPolygon2D
-@onready var polygon_2d = $Past/CollisionPolygon2D/Polygon2D
 var paused = false
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	pause_menu.hide()
-	polygon_2d.polygon = collision_polygon_2d.polygon
 
 func _process(delta):
 	if Input.is_action_pressed("ui_cancel"):
