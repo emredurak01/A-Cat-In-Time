e9736f0b25f7208e69ab15f02d8393e09501b00b
Emre Durak <emredurak01@gmail.com>
2024-03-11 03:16:53 +0300

    door

--- Prefab/door.gd
+++ Prefab/door.gd
@@ -1,8 +1,19 @@
 extends StaticBody2D
 
+func _ready():
+	$closed.visible = true
+	$out.visible = false
+	$in.visible = false
+
 func _process(delta):
-	if(find_child("pressure_plate").pressed):
+	if(find_child("pressure_plate").pressed and find_child("pressure_plate2").pressed):
 		set_collision_layer_value(2, false)
+		$closed.visible = false
+		$out.visible = true
+		$in.visible = true
 	else:
 		set_collision_layer_value(2, true)
+		$closed.visible = true
+		$out.visible = false
+		$in.visible = false
 	
